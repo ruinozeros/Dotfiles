c5576c0190beedd4b95241f1ea9691f879d228b3
Thomas Ruess <thomas.ruess@posteo.net>
2020-10-12 21:49:33 +0200

    suppress output

--- zsh/install_oh_my_zsh.sh
+++ zsh/install_oh_my_zsh.sh
@@ -10,8 +10,14 @@ success=0
 if [ -d $HOME/.oh-my-zsh ]; then
     echo oh-my-zsh is already installed!
 else
-    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" > /dev/null
     success=$?
+
+    if [ $success -eq 0 ]; then 
+        echo oh-my-zsh successfully installed!
+    else 
+        echo oh-my-zsh installation failed!
+    fi
 fi
 
 return $success
